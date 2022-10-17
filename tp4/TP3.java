7d103af53e43bc18686f46266e00ff714d1032c1
saputraari11 <saputra_ari_28rpl@student.smktelkom-mlg.sch.id>
2022-10-17 12:33:06 +0700

    remake tp4

--- tp4/TP3.java
+++ tp4/TP3.java
@@ -3,18 +3,15 @@ import java.util.*;
 public class TP3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        HashMap<String,Integer> studentScore = new HashMap<String,Integer>();      
-        studentScore.put("Dira", input.nextInt() );
-        studentScore.put("Radi", input.nextInt());
-        studentScore.put("Idar", input.nextInt());
-        String maxScore = "";
-        int score = 0;
-        for(Map.Entry item:studentScore.entrySet()){
-            if(score <= (int) item.getValue()) {
-                score = (int) item.getValue();
-                maxScore = (String) item.getKey();
-            }
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if((a>b) && (a>c)) {
+            System.out.println("Dira");
+        } else if((b>a) && (b>c)) {
+            System.out.println("Radi");
+        } else {
+            System.out.println("Idar");
         }
-        System.out.println(maxScore);
     }
 }
